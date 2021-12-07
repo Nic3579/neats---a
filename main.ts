4ffa6276f417df6ce3b7a122f72a0e46d7397f47
Nic3579 <92817235+Nic3579@users.noreply.github.com>
2021-12-07 14:17:13 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -13,12 +13,25 @@ let Alarm_on = 0
 let countdown = 0
 radio.setGroup(17)
 countdown = 10
+let security_enabled = 1
+pins.setAudioPin(AnalogPin.P0)
+music.setVolume(255)
 loops.everyInterval(1000, function () {
     countdown += -1
-    if (countdown == 0) {
+    if (countdown <= 0) {
         Alarm_on = 1
     }
 })
 basic.forever(function () {
-	
+    // alarm
+    if (Alarm_on == 1 && security_enabled == 1) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+        music.playMelody("E G - E G - E G ", 120)
+    }
 })
