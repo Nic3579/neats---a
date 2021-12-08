9b3e28fc29a0841b89c59b115a74ed96df3fe6d8
Nic3579 <92817235+Nic3579@users.noreply.github.com>
2021-12-08 17:22:28 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,8 +5,13 @@ input.onButtonPressed(Button.B, function () {
 	
 })
 radio.onReceivedValue(function (name, value) {
-    if (name == "cb269") {
+    // this verifies the microbit and resets the timer while leading in to the other code message
+    if (value == 741852639321564900) {
         countdown = 60
+        // if the character for the first system is correct, turn on the light and turn on the alarm
+        if ("" == "") {
+            Alarm_on = 1
+        }
     }
 })
 let Alarm_on = 0
@@ -16,6 +21,7 @@ countdown = 10
 let security_enabled = 1
 pins.setAudioPin(AnalogPin.P0)
 music.setVolume(255)
+// this counts down on a timer to make sure the other micro:bit is on
 loops.everyInterval(1000, function () {
     countdown += -1
     if (countdown <= 0) {
