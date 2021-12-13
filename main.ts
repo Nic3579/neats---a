693f1c05aefedcc2d92793c03cb008da82bbd60e
Nic3579 <92817235+Nic3579@users.noreply.github.com>
2021-12-13 14:12:57 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,27 +1,40 @@
 input.onButtonPressed(Button.A, function () {
-	
+    Alarm_on = 0
+    radio.sendString("alarmoff")
+})
+radio.onReceivedString(function (receivedString) {
+    countdown = 60
+    if (receivedString == "ALARM") {
+        Alarm_on = 1
+    }
+    if (receivedString == "ALARMOFF") {
+        Alarm_on = 0
+    }
+    if (receivedString == "ENABLE") {
+        security_enabled = 1
+    }
+    if (receivedString == "DISABLE") {
+        security_enabled = 0
+    }
 })
 input.onButtonPressed(Button.B, function () {
-	
-})
-radio.onReceivedValue(function (name, value) {
-    // this verifies the microbit and resets the timer while leading in to the other code message
-    if (value == 741852639321564900) {
-        countdown = 60
-        // if the character for the first system is correct, turn on the light and turn on the alarm
-        if ("" == "") {
-            Alarm_on = 1
-        }
+    if (security_enabled == 1) {
+        security_enabled = 0
+        radio.sendString("ENABLED")
+    } else {
+        security_enabled = 1
+        radio.sendString("DISABLED")
     }
 })
 let Alarm_on = 0
+let security_enabled = 0
 let countdown = 0
 radio.setGroup(17)
-countdown = 10
-let security_enabled = 1
+countdown = 60
+security_enabled = 0
+Alarm_on = 0
 pins.setAudioPin(AnalogPin.P0)
 music.setVolume(255)
-// this counts down on a timer to make sure the other micro:bit is on
 loops.everyInterval(1000, function () {
     countdown += -1
     if (countdown <= 0) {
