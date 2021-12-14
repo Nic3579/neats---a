fff03af4ac3177d11e87dd1f4117c1c75ea9b6bf
Nic3579 <92817235+Nic3579@users.noreply.github.com>
2021-12-13 23:15:21 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,17 @@
 input.onButtonPressed(Button.A, function () {
     Alarm_on = 0
-    radio.sendString("alarmoff")
+    countdown = 30
+    radio.sendString("ALARMOFF")
+    basic.showLeds(`
+        # . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . #
+        `)
 })
 radio.onReceivedString(function (receivedString) {
-    countdown = 60
+    countdown = 30
     if (receivedString == "ALARM") {
         Alarm_on = 1
     }
@@ -17,28 +25,47 @@ radio.onReceivedString(function (receivedString) {
         security_enabled = 0
     }
 })
+// this code toggles on or off weather the alarm is enabled when button B is pressed
 input.onButtonPressed(Button.B, function () {
+    // allows toggling weather or not the alarm is enabled
     if (security_enabled == 1) {
         security_enabled = 0
-        radio.sendString("ENABLED")
+        radio.sendString("DISABLED")
     } else {
         security_enabled = 1
-        radio.sendString("DISABLED")
+        radio.sendString("ENABLED")
     }
+    basic.showLeds(`
+        . # . # .
+        # . . . #
+        . . . . .
+        # . . . #
+        . # . # .
+        `)
 })
 let Alarm_on = 0
 let security_enabled = 0
 let countdown = 0
 radio.setGroup(17)
-countdown = 60
+countdown = 10
 security_enabled = 0
 Alarm_on = 0
 pins.setAudioPin(AnalogPin.P0)
 music.setVolume(255)
+// this is a part of the pinging system; every 30 sec. the other micro:bit sends a message and if it doesn't respond within 60 sec of the last time it sent a message, the alarm goes off
 loops.everyInterval(1000, function () {
-    countdown += -1
-    if (countdown <= 0) {
-        Alarm_on = 1
+    if (security_enabled == 1) {
+        countdown += -1
+        if (countdown <= 0) {
+            Alarm_on = 1
+            basic.showLeds(`
+                . . . . .
+                . # . # .
+                . . # . .
+                . # . # .
+                . . . . .
+                `)
+        }
     }
 })
 basic.forever(function () {
@@ -49,7 +76,7 @@ basic.forever(function () {
         . . . . .
         . . . . .
         `)
-    // alarm
+    // this code puts on an alarm sound when the alarm is on (alarm on variable = 1 and enabled variable = 1)
     if (Alarm_on == 1 && security_enabled == 1) {
         basic.showLeds(`
             . . . . .
